7ef1e5b38171e6fd6f19b4d3a92a24f56360f8fa
Lee486 <lee.suah1998@gmail.com>
2022-01-24 15:03:33 -0500

    adding char data

---
+++ Fate--Grand-Order-Servant-Data-Extractor-master/.ipynb_checkpoints/FGO_Data_Extractor-checkpoint.py
@@ -0,0 +1,113 @@
+import bs4 as bs
+import requests
+import pandas as pd
+import numpy as np
+import time
+
+Servant_Data = []
+
+column = ['Name','Alias','Class','ID','Rarity','Drain','Max Lvl.','ATK lvl. 1','HP lvl. 1','ATK lvl. at MAX Servant level',
+          'HP lvl. at MAX Servant level','ATK lvl. 90','HP lvl. 90','ATK lvl. 100','HP lvl. 100'
+    ,'MAX ATK + Fou','MAX HP + Fou','NP gain','Quick Card Hits','Star Weight','Arts Card Hits','Star Rate',
+         'Buster Card Hits','Death Rate','Extra Attack Hits','Attribute','Noble Phantasm Hits','Traits','Height and Weight','Alignment','Series','Gender'
+         ,'Origin','Region']
+
+rarity = {'★★★ R':'3-Star','★★★★★ SSR':'5-star','★★★★ SR':'4-Star','★★':'2-Star','★':'1-Star','---':'2-Star'}
+
+Servant_count = 265
+
+
+for i in range(1,Servant_count):
+   servdata = []
+   'Numbers for Tiamat,Goetia,Solomon and Beast III(L/R) excluded'
+   if( i in [149,151,152,168,239]):
+      continue
+   else:
+
+    print('Current Servant count : ',i)
+
+   'HTTP request to Cirnopedia website'
+   req = requests.get('http://fate-go.cirnopedia.org/servant_profile.php?servant=' + str(i),headers={'User Agent': 'Mozilla/5.0'})
+   soup = bs.BeautifulSoup(req.content,'lxml')
+
+   'Get contents of the tables in the Servant Profile'
+   table = soup.find_all('tbody')
+
+   'Data of the Status table'
+   table_rows1 = table[0].find_all('tr')
+
+   for tr in table_rows1:
+    td_desc = tr.find_all('td', class_='desc')
+    servant_table1 = [str(st1.text.strip()) for st1 in td_desc]
+    servdata.extend(servant_table1)
+   if (len(servdata) < 14):
+       grailval590 = servdata[9:11]
+       servdata.insert(11,grailval590[0])
+       servdata.insert(12,grailval590[1])
+       #print(servdata)
+       Servant_Data.extend(servdata)
+       #print(Servant_Data)
+   else:
+       Servant_Data.extend(servdata)
+
+
+   'Data of the Hidden Stats table'
+   table_rows2 = table[1].find_all('tr')
+
+   for tr in table_rows2:
+    td_desc = tr.find_all('td',class_='desc')
+    servant_table2 = [str(st2.text.strip()) for st2 in td_desc]
+    Servant_Data.extend(servant_table2)
+
+   'Data from the Background 1 table (excluding comment)'
+   table_row_9 = table[9].find_all('tr')[:3]
+
+   for tr in table_row_9:
+     td_desc = tr.find_all('td',class_='desc')
+     servant_table3 = [str(st3.text.strip())for st3 in td_desc]
+     Servant_Data.extend(servant_table3)
+
+#print(Servant_Data)
+
+print(len(Servant_Data))
+
+'Create a dataframe to store the values and columns'
+
+df = pd.DataFrame(np.array(Servant_Data).reshape(Servant_count-6,32))
+#df = pd.DataFrame(Servant_Data, columns=column)
+print(df)
+
+
+df = df[['ID','Name','Alias','Class','Rarity','Drain','Max Lvl.','ATK lvl. 1','HP lvl. 1','ATK lvl. at MAX Servant level',
+         'HP lvl. at MAX Servant level','ATK lvl. 90','HP lvl. 90','ATK lvl. 100','HP lvl. 100','NP gain','Star Weight','Star Rate','Death Rate','Buster Card Hits',
+         'Arts Card Hits','Quick Card Hits', 'Extra Attack Hits','Attribute','Noble Phantasm Hits','Traits','Height and Weight','Alignment','Series','Gender',
+         'Origin','Region']]
+
+new_NP = df['NP gain'].str.split('・',n=1,expand=True)
+df['ATK NP gain'] = new_NP[0]
+df['DEF NP gain'] = new_NP[1]
+df['ATK NP gain'] = df['ATK NP gain'].replace("Attack: "," ",regex=True)
+df['DEF NP gain'] = df['DEF NP gain'].replace("Defense: "," ",regex=True)
+
+HandW = df['Height and Weight'].str.split('・',n=1,expand=True)
+df['Height'] = HandW[0]
+df['Weight'] = HandW[1]
+
+df['Buster Card Hits'] = df['Buster Card Hits'].replace('Hits'," ",regex=True)
+df['Arts Card Hits'] = df['Arts Card Hits'].replace('Hits'," ",regex=True)
+df['Quick Card Hits'] = df['Quick Card Hits'].replace('Hits'," ",regex=True)
+df['Noble Phantasm Hits'] = df['Noble Phantasm Hits'].replace('Hits'," ",regex=True)
+df['Extra Attack Hits'] = df['Extra Attack Hits'].replace('Hits'," ",regex=True)
+
+df['Rarity'] = df['Rarity'].replace(rarity)
+
+df = df[['ID','Name','Alias','Class','Rarity','Drain','Max Lvl.','ATK lvl. 1','HP lvl. 1',
+           'ATK lvl. at MAX Servant level','HP lvl. at MAX Servant level','ATK lvl. 90','HP lvl. 90','ATK lvl. 100','HP lvl. 100',
+          'ATK NP gain','DEF NP gain','Star Weight','Star Rate','Death Rate','Buster Card Hits','Arts Card Hits','Quick Card Hits',
+          'Extra Attack Hits','Noble Phantasm Hits','Attribute','Traits','Height','Weight','Alignment','Series','Gender','Origin','Region']]
+
+'Write to a .csv file'
+
+df.to_csv("FGO_Servant_Data.csv",encoding = 'utf-8-sig',index=False)
+#df.to_json("FGO_Servant_Data.json",orient='split');
+
--- extra.md
+++ extra.md
@@ -1,1 +1,73 @@
 # what
+
+>>endeku: where Hawks sees how gentle Endeavor has become?
+
+Hawks want it to be written somewhere and down for the record, okay? He was the original TodoDeku shipper. 
+
+# Content Creators
+
+>> background apocalypse & eri + kouta are the only publishing mangaka
+
+Summary: Eri huffed and placed her hands on her hips, "Well, when you can rub enough braincells together to write your own story, you can write what you want."
+
+Kouta glared back. "One day," he seethed back, snatching the manuscript out of her hands, "One fucking day."
+
+-
+
+There are 8 Twices walking in and out of the same room. And that's how the rest of the base knows.
+
+The next chapter is about to be released.
+
+-
+
+"And you're okay with this?" Hawks asked, waving the dj in front of Midoriya's face.
+
+Midoriya stared at the doujin, a drawn picture of him in a rauchy bunny-suit looking up at the camera, and he sighed.
+
+"Do you remember?" he asked, "When they first came in?"
+
+Hawks frowned, and Midoriya looked back forward.
+
+"I remember. Eri threw up everything she ate and cried all night. Kouta never spoke and never moved, and if anyone touched him, they'd be scratched and bitten."
+
+The blond stared and looked down. He remembered. The first children they had seen in months, living like rats. They found Eri after Kouta, and at least they were better prepared for her, but Kouta especially was hard. Hawks remembered, one several occasions, some of the others recommending to just put him out of his misery.
+
+"But now they know how to read and write. They laugh and smile and they're working hard for a future," he replied. He took the doujinshi and took it out of Hawks' hand. Looking down at it, he gave a resigned smile with a shrug, "It's fiction. They're still good kids. And you know, base has been a little more lively recently."
+
+Hawks stared at him for a moment and crossed his arms over his chest. Talking to Midoriya like this made him feel like the world was a bright and beautiful place.
+
+"Fuck you, maid's are the best, you furry!"
+
+"Shut up, you stupid earthworm! It's not furry, it's scaley!"
+
+Midoriya smile kept its place, and Hawks didn't think he would ever meet someone as strong as him.
+
+-
+
+"Chisa-ji is not nearly as bad as you write him out to be," Kouta said.
+
+Eri gave a sharp cry, sounding more like a yowl-like someone pulled the plug on Shigaraki's game again- and turned to Kouta with the promise of pain in her eyes.
+
+"You didn't know him pre-Izu-nii."
+
+Kouta rolled his eyes, "Yeah, but child experimentation? Come on, that's such a gimmie. Why can't you make him 3-D instead of some Jesus freak?"
+
+Eri flushed holty, "Augh! You're so stupid, Kouta! It's just a character quirk! You said you wanted to try with the religious scriptures!"
+
+"I never said that!" Kouta shouted right back. "Just that altar-sex would be fun to try!"
+
+"Same difference! You just want to summon another demon, you monster-fucker!"
+
+"You leave Chimera-san out of this!"
+
+-
+
+"So uh..." Yamada coughed into his fist, looked up and down the hallway, and then leaned in closer, "Any chance on some Present Mic/Deku?"
+
+Kouta gave him this dead-eyed look, and as though he was just counting down to the last minute where he didn't have to be here anymore, pointed at the 
+
+-
+
+And then the library system was created and there was a new acknoledgement.
+
+No one wanted to borrow a book after Shigaraki.
